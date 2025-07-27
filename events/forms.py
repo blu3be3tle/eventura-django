9c3d0353f0db97e28c29df97c9fd34c81d474609
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-07-27 19:45:57 +0600

    forms updated

--- events/forms.py
+++ events/forms.py
@@ -1,10 +1,23 @@
 from django import forms
-from .models import Event, Participant
+from .models import Category, Event, Participant
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'description']
+
 
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['name', 'description', 'date', 'location', 'category']
+        fields = ['name', 'description', 'date',
+                  'time', 'location', 'category']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'time': forms.TimeInput(attrs={'type': 'time'}),
+        }
+
 
 class ParticipantForm(forms.ModelForm):
     class Meta:
