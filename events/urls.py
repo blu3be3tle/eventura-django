c9418be4ebd5973a9d4504af45b0a2f2a022717b
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-08-04 22:21:36 +0600

    added list views for categories

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
+    # Event
     path('', views.event_list, name='event-list'),
     path('event/<int:pk>/', views.event_detail, name='event-detail'),
     path('event/new/', views.event_create, name='event-create'),
@@ -10,6 +12,8 @@ urlpatterns = [
     path('organizer-dashboard/', views.organizer_dashboard, name='dashboard'),
     path("__reload__/", include("django_browser_reload.urls")),
 
+
+    # Participant
     path('participants/', views.participant_list, name='participant-list'),
     path('participant/<int:pk>/', views.participant_detail,
          name='participant-detail'),
@@ -20,4 +24,11 @@ urlpatterns = [
          views.participant_delete, name='participant-delete'),
 
 
+    # Category
+    path('categories/', views.category_list, name='category-list'),
+    path('category/<int:pk>/', views.category_detail, name='category-detail'),
+    path('category/new/', views.category_create, name='category-create'),
+    path('category/<int:pk>/edit/', views.category_update, name='category-update'),
+    path('category/<int:pk>/delete/',
+         views.category_delete, name='category-delete'),
 ]
