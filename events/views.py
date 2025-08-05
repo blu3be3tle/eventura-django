5c16346089f052256ed68f6c9a0e14ab737c956a
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-08-05 22:39:40 +0600

    moved dashboard views to dashboard app

--- events/views.py
+++ events/views.py
@@ -85,47 +85,7 @@ def event_delete(request, pk):
     return render(request, 'events/event_delete.html', context)
 
 
-# Dashboard
-def organizer_dashboard(request):
-
-    total_users = User.objects.count()
-    total_events = Event.objects.count()
-
-    now = timezone.now()
-    upcoming_events_count = Event.objects.filter(date__gte=now.date()).count()
-    past_events_count = Event.objects.filter(date__lt=now.date()).count()
-
-    event_filter = request.GET.get('filter', 'upcoming')
-    events_to_display = Event.objects.select_related('category')
-    users_to_display = User.objects.none()
-
-
-    if event_filter == 'users':
-        users_to_display = User.objects.all()
-    if event_filter == 'past':
-        events_to_display = events_to_display.filter(date__lt=now.date())
-    elif event_filter == 'today':
-        events_to_display = events_to_display.filter(date=now.date())
-    elif event_filter == 'upcoming':
-        events_to_display = events_to_display.filter(date__gte=now.date())
-
-    context = {
-        'total_users': total_users,
-        'total_events': total_events,
-        'upcoming_events_count': upcoming_events_count,
-        'past_events_count': past_events_count,
-        'today_events': Event.objects.filter(date=now.date()),
-        'events_to_display': events_to_display,
-        'users_to_display': users_to_display,
-        'current_filter': event_filter,
-    }
-    return render(request, 'events/organizer_dashboard.html', context)
-
-
-
 # Category
-
-
 def category_create(request):
     if request.method == 'POST':
         form = CategoryForm(request.POST)
