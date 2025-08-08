79db67f70f34ef56444924db93c607477668f451
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-08-08 14:02:38 +0600

    user registration form created

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import User
 from events.models import Event
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class UserForm(forms.ModelForm):
@@ -13,3 +15,16 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['name', 'email', 'events']
+    
+
+
+class RegisterForm(UserCreationForm):
+
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
