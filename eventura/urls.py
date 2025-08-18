17162dc7f8ec7c17a47eac3712857b5bbc46b2d2
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-08-18 12:19:51 +0600

    no permission url added

--- eventura/urls.py
+++ eventura/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
+from dashboard.views import no_permission
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,5 +19,6 @@ if settings.DEBUG:
         path('__debug__/', include(debug_toolbar.urls)),
         path("__reload__/", include("django_browser_reload.urls")),
     ]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
     urlpatterns += staticfiles_urlpatterns()
