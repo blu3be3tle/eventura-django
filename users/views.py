0dfba95136a9887591a276b79a3aa5078f6de60c
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-08-23 00:00:43 +0600

    added profile and related views

--- users/views.py
+++ users/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from .forms import EditProfileForm, EditProfilePictureForm, CustomPasswordChangeForm, SignUpForm, LoginForm
+from django.shortcuts import render, redirect, HttpResponse
+from django.views import View
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
-from users.forms import SignUpForm, LoginForm
 
 # Sign up
+
+
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -49,3 +55,51 @@ def activate_user(request, user_id, token):
     else:
         return render(request, 'registration/activation_invalid.html')
 
+
+# Profile
+class ProfileView(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'users/profile/profile.html')
+
+
+class EditProfileView(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        profile_form = EditProfileForm(instance=request.user)
+        picture_form = EditProfilePictureForm(instance=request.user.profile)
+        context = {
+            'profile_form': profile_form,
+            'picture_form': picture_form,
+        }
+        return render(request, 'users/profile/edit_profile.html', context)
+
+    def post(self, request, *args, **kwargs):
+        profile_form = EditProfileForm(request.POST, instance=request.user)
+        picture_form = EditProfilePictureForm(
+            request.POST, request.FILES, instance=request.user.profile)
+
+        if profile_form.is_valid() and picture_form.is_valid():
+            profile_form.save()
+            picture_form.save()
+            messages.success(
+                request, 'Your profile has been updated successfully!')
+            return redirect('profile')
+
+        context = {
+            'profile_form': profile_form,
+            'picture_form': picture_form,
+        }
+        return render(request, 'users/profile/edit_profile.html', context)
+
+
+class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+
+    form_class = CustomPasswordChangeForm
+    template_name = 'users/profile/change_password.html'
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        messages.success(
+            self.request, 'Your password was successfully updated!')
+        return super().form_valid(form)
