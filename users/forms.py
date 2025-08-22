dd16fbc2a18baa2bd26a86375618a92c358fbbe8
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-08-22 19:59:52 +0600

    created forms for edit profile, pw and img change

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Profile
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from django.contrib.auth.models import User, Group
 from django import forms
 
@@ -16,3 +17,24 @@ class SignUpForm(UserCreationForm):
 
 class LoginForm(AuthenticationForm):
     pass
+
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class EditProfilePictureForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('profile_picture', 'phone_number')
+
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update(
+                {'class': 'mt-1 block w-full p-2 border border-gray-300 rounded-md'})
