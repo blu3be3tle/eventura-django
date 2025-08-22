4e9c22b49ce7f821f94aab84103d277100157c99
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-08-22 19:37:29 +0600

    automated profile creation

--- users/signals.py
+++ users/signals.py
@@ -1,9 +1,7 @@
+from .models import Profile
 from events.models import Event
-from django.template.loader import render_to_string
-from django.db.models.signals import m2m_changed
-from django.db.models.signals import post_save
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
 from django.conf import settings
@@ -59,3 +57,9 @@ def rsvp_notification(sender, instance, action, pk_set, **kwargs):
                 "Thanks,\nThe Eventura Team"
             )
             send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email])
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
